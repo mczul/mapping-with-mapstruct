ebcd0e4f31f8abb4905cd2f617cad5419723c3c6
Christoph Zuleger <c.zuleger@cronos.de>
2022-09-08 23:37:50 +0200

    Improve documentation

--- src/main/java/de/cronos/demo/mapping/api/model/StatisticsMapper.java
+++ src/main/java/de/cronos/demo/mapping/api/model/StatisticsMapper.java
@@ -35,13 +35,21 @@ public abstract class StatisticsMapper {
         return "X".equalsIgnoreCase(source);
     }
 
+    // default MapStruct conversion from String to Boolean
     @Mapping(target = "secure", source = SOURCE_FIELD_SHOP_SECURE)
+    // Shop specific conversion from String to Boolean
     @Mapping(target = "open", source = SOURCE_FIELD_SHOP_OPEN, qualifiedBy = {ShopBoolean.class})
+    // SAP specific conversion from String to Boolean
     @Mapping(target = "sapWorking", source = SOURCE_FIELD_SAP_ACTIVE, qualifiedBy = {SapBoolean.class})
+    // Java expression as the source value
     @Mapping(target = "products.total", expression = "java(productRepository.count())")
+    // Constant string to target (including automatic type conversion)
     @Mapping(target = "products.quality", constant = "wow!")
+    // Map key as the source
     @Mapping(target = "products.lastComplainReceived", source = SOURCE_FIELD_LAST_COMPLAIN)
+    // default MapStruct conversion from String to LocalDateTime
     @Mapping(target = "proudSince", constant = "1970-01-01T12:34:56")
+    // default MapStruct conversion from OffsetDateTime as string to Instant
     @Mapping(target = "soldOutUntil", constant = "2022-12-31T23:59:59+10:00")
     public abstract ShopStatistics basedOn(Map<String, String> source);
 
