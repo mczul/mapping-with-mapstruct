1e7144c53c145fb1e95c2edff4d12635b4293d83
Christoph Zuleger <c.zuleger@cronos.de>
2022-10-11 23:08:00 +0200

    - Explicit table name configuration for Liquibase - Switch database port to prevent local collisions with other PostgreSQL processes - Improve immutable JPA entity projections (nested properties and usage of Lombok's @Value feature

--- src/main/java/de/cronos/demo/mapping/products/statistics/ProductStatistics.java
+++ src/main/java/de/cronos/demo/mapping/products/statistics/ProductStatistics.java
@@ -1,48 +1,52 @@
 package de.cronos.demo.mapping.products.statistics;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.Value;
 import org.hibernate.annotations.Immutable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-@NoArgsConstructor
-@Getter
+@Value
 @EqualsAndHashCode(of = {"id"})
 @ToString
-@Entity
 @Immutable
+@Entity
 @Table(name = "product_statistics")
 public class ProductStatistics {
 
+    @Embeddable
+    @Value
+    public static class Orders {
+
+        @Column(name = "last_successful_order")
+        Instant lastSuccessful = null;
+
+        @Column(name = "average_order_quantity")
+        BigDecimal averageQuantity = null;
+
+        @Column(name = "successful_orders")
+        Long successful = null;
+
+    }
+
     @Id
     @Column(name = "id")
-    UUID id;
+    UUID id = null;
 
     @Column(name = "name")
-    String name;
+    String name = null;
 
     @Column(name = "created")
-    Instant created;
+    Instant created = null;
 
     @Column(name = "last_modified")
-    Instant lastModified;
+    Instant lastModified = null;
 
-    @Column(name = "last_successful_order")
-    Instant lastSuccessfulOrder;
-
-    @Column(name = "average_order_quantity")
-    BigDecimal averageOrderQuantity;
-
-    @Column(name = "successful_orders")
-    Long successfulOrders;
+    @Embedded
+    Orders orders = null;
 
 }
