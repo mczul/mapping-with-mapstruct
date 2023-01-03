dc4d793363d42cccb1e213b72848a3005d5d8aef
Christoph Zuleger <c.zuleger@cronos.de>
2023-01-03 18:38:26 +0100

    - Extend QueryOrderEvent with bean validation samples - Extend QueryOrderEvent with new param representing a list of product tag ids that must be referenced by all matching orders; to be precise: all matching orders must reference products that reference at least the provided tags) - Update from Spring Boot 2.7.4 to 2.7.5

--- src/main/java/de/cronos/demo/mapping/orders/OrderRepository.java
+++ src/main/java/de/cronos/demo/mapping/orders/OrderRepository.java
@@ -2,6 +2,8 @@ package de.cronos.demo.mapping.orders;
 
 import de.cronos.demo.mapping.customers.CustomerEntity_;
 import de.cronos.demo.mapping.orders.events.QueryOrderEvent;
+import de.cronos.demo.mapping.products.ProductEntity_;
+import de.cronos.demo.mapping.tags.TagEntity_;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -41,6 +43,23 @@ public interface OrderRepository extends JpaRepository<OrderEntity, UUID>, JpaSp
             queryOrderEvent.getCustomerMail().ifPresent(mail -> predicates.add(
                     criteriaBuilder.like(root.get(OrderEntity_.customer).get(CustomerEntity_.email), "%" + mail + "%"))
             );
+
+            // Referenced tags
+            queryOrderEvent.getReferencedTagIds().forEach(tagId -> {
+                final var existsSubquery = query.subquery(String.class);
+                final var subRoot = existsSubquery.from(OrderEntity.class);
+                final var subTagJoin = subRoot.join(OrderEntity_.product)
+                        .join(ProductEntity_.tags);
+                existsSubquery.select(criteriaBuilder.literal("x"));
+                existsSubquery.where(
+                        criteriaBuilder.and(
+                                criteriaBuilder.equal(subRoot.get(OrderEntity_.id), root.get(OrderEntity_.id)),
+                                criteriaBuilder.equal(subTagJoin.get(TagEntity_.id), tagId)
+                        )
+                );
+
+                predicates.add(criteriaBuilder.exists(existsSubquery));
+            });
 
             return predicates.stream().reduce(
                     criteriaBuilder.conjunction(),
--- src/main/java/de/cronos/demo/mapping/orders/events/QueryOrderEvent.java
+++ src/main/java/de/cronos/demo/mapping/orders/events/QueryOrderEvent.java
@@ -5,16 +5,26 @@ import lombok.Builder;
 import lombok.Value;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Value
 @Builder
 public class QueryOrderEvent {
 
     @NotNull
-    Optional<String> customerMail;
+    Optional<@Size(min = 3) String> customerMail;
 
     @NotNull
     Optional<OrderState> orderState;
+
+    /**
+     * Caution: All of the tag ids must be assigned to a referenced product
+     */
+    @NotNull
+    List<UUID> referencedTagIds = new ArrayList<>();
 
 }
